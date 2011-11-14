af3e91a3c50cf4eee5613526c0209cbd1846e923
Felix Geisendörfer <felix@debuggable.com>
2011-11-14 13:39:30 +0100

    Implement FileFinder

---
+++ test/integration/FileFinder/test-finds-all-fixtures.js
@@ -0,0 +1,23 @@
+var assert     = require('assert');
+var FileFinder = require('../../../lib/FileFinder');
+var path       = require('path');
+var fixture    = path.join(__dirname, '../../fixture');
+
+var finder = new FileFinder(fixture);
+var files  = [];
+finder.execute(function(err, _files) {
+  if (err) throw err;
+  files = _files;
+});
+
+process.on('exit', function() {
+  assert.deepEqual(files, [
+    fixture + '/1.txt',
+    fixture + '/2.js',
+    fixture + '/a/a1.txt',
+    fixture + '/a/a2.js',
+    fixture + '/b/b1.txt',
+    fixture + '/b/b2.js',
+    fixture + '/b/deep/file.js',
+  ]);
+});
